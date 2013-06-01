b04f74d8588d12fa01566c440fbea737f27b45be
T Beseda <tbeseda@gmail.com>
2013-06-01 15:46:19 -0600

    relationships

--- app/models/disaster_event.rb
+++ app/models/disaster_event.rb
@@ -1,3 +1,5 @@
 class DisasterEvent < ActiveRecord::Base
+  has_and_belongs_to_many :recovery_resources
+
   attr_accessible :name
 end
--- app/models/recovery_resource.rb
+++ app/models/recovery_resource.rb
@@ -1,3 +1,7 @@
 class RecoveryResource < ActiveRecord::Base
+  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :disaster_events
+  has_and_belongs_to_many :entity_type_available_tos
+
   attr_accessible :name
 end
