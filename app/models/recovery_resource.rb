7bed0de5075b5305444e64dacddcedbbb75e79a2
T Beseda <tbeseda@gmail.com>
2013-06-02 11:22:07 -0600

    allow assignment of location data for resources

--- app/models/recovery_resource.rb
+++ app/models/recovery_resource.rb
@@ -3,6 +3,6 @@ class RecoveryResource < ActiveRecord::Base
   has_and_belongs_to_many :disaster_events
   has_and_belongs_to_many :entity_type_available_tos
 
-  attr_accessible :name, :category_ids, :disaster_event_ids, :entity_type_available_to_ids
+  attr_accessible :name, :category_ids, :disaster_event_ids, :entity_type_available_to_ids, :national, :state, :locale
 
 end
