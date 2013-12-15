8e19bb6769f126bdc143278e8e6e93fc78770766
Rylan Bowers <rylanb@gmail.com>
2013-12-15 15:29:06 -0700

    Adding basic tests to the RR Draft model.

--- spec/models/recovery_resource_draft_spec.rb
+++ spec/models/recovery_resource_draft_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe RecoveryResourceDraft do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_and_belong_to_many :categories }
+  it { should have_and_belong_to_many :disaster_events }
+  it { should have_and_belong_to_many  :entity_type_available_tos }
+
+
+  it { should allow_mass_assignment_of :description }
+  it { should allow_mass_assignment_of :locale }
+  it { should allow_mass_assignment_of :name }
+  it { should allow_mass_assignment_of :national }
+  it { should allow_mass_assignment_of :published }
+  it { should allow_mass_assignment_of :recovery_resource_id }
+  it { should allow_mass_assignment_of :state }
 end
