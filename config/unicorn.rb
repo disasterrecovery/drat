8e0a95f4f4d027f4ba8ca37d6bcc04a930d8eea2
Rylan Bowers <rylanb@gmail.com>
2014-02-15 16:26:08 -0700

    Updating unicorn config and deploy tasks.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,81 @@
-if ENV["RAILS_ENV"] == "development"
+if ENV["RAILS_ENV"].nil? || ENV["RAILS_ENV"] == "development"
   worker_processes 1
+  timeout 30
 else
-  worker_processes 3
-end
 
-timeout 30
+  # timeout 30
+  # My pimped out unicorn config, with incremental killof
+  # and all the latest capistrano & bundler-proofing
+  # @jamiew :: http://github.com/jamiew
+
+  application = "drat"
+  environment = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'production'
+  app_path = "/home/rails/#{application}"
+  bundle_path = "#{app_path}/shared/bundle"
+
+  timeout 30
+  preload_app true
+  worker_processes 8
+
+  # Listen on both fast-failing unix data socket & a backloggable TCP connection
+  #listen "/srv/#{application}/shared/tmp/sockets/unicorn.sock", :backlog => 64
+  listen 8080, :backlog => 256
+
+  # Hard-set the CWD & pidfile to ensure app-reloading consistency
+  working_directory "#{app_path}/current"
+  #stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+  #pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+
+  # Helps ensure the correct unicorn binary is used when upgrading with USR2
+  # See http://unicorn.bogomips.org/Sandbox.html
+  # Unicorn::HttpServer::START_CTX[0] = "#{bundle_path}/bin/unicorn"
+
+  # Rails breaks unicorn's logger formatting, reset it
+  # http://rubyforge.org/pipermail/mongrel-unicorn/2010-October/000732.html
+  Unicorn::Configurator::DEFAULTS[:logger].formatter = Logger::Formatter.new
+
+  # Forcibly clean environment variables between bundlings
+  # http://www.mail-archive.com/mongrel-unicorn@rubyforge.org/msg00276.html
+  before_exec do |_|
+    paths = (ENV["PATH"] || "").split(File::PATH_SEPARATOR)
+    paths.unshift "#{bundle_path}/bin"
+
+    ENV["PATH"] = paths.uniq.join(File::PATH_SEPARATOR)
+    ENV["GEM_HOME"] = ENV['GEM_PATH'] = bundle_path
+    ENV["BUNDLE_GEMFILE"] = "/home/rails/#{application}/current/Gemfile"
+  end
+
+
+  before_fork do |server, worker|
+    ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
+
+    # Kill the old master as soon as our 1st worker is ready
+    # old_pid = "/srv/#{application}/shared/tmp/pids/unicorn.pid.oldbin"
+    # if File.exists?(old_pid) && server.pid != old_pid
+    #   begin
+    #     Process.kill("QUIT", File.read(old_pid).to_i)
+    #   rescue Errno::ENOENT, Errno::ESRCH
+    #     # someone else killed the old master already
+    #   end
+    # end
+
+    # Incremental kill-off
+    # old_pid = "#{server.config[:pid]}.oldbin"
+    # if old_pid != server.pid
+    #   begin
+    #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+    #     puts "Sending #{sig} signal to old unicorn master..."
+    #     Process.kill(sig, File.read(old_pid).to_i)
+    #   rescue Errno::ENOENT, Errno::ESRCH
+    #   end
+    # end
+
+    # Throttle the master from forking too quickly (for incremental kill-off only)
+    #sleep 1
+  end
+
+  after_fork do |server, worker|
+    #NewRelic::Agent.shutdown
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+  end
+end
