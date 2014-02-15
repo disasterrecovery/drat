4b36a0e6f03d312dadcbcc2d0d00b8b9684f97ea
Rylan Bowers <rylanb@gmail.com>
2014-02-15 15:37:01 -0700

    Adding basic unicorn config. Setting up deploy scripts.

---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+require 'capistrano-unicorn'
+
+set :application, 'drat'
+set :repo_url, 'git@github.com:tbeseda/drat.git'
+
+#ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :branch, 'master'
+set :deploy_to, '/home/rails/drat'
+#set :scm, :git
+set :format, :pretty
+set :log_level, :debug
+# set :pty, true
+
+set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+set :rvm_ruby_string, 'ruby-1.9.3'
+set :rvm_bin_path, '/usr/local/rvm/bin'
+set :rvm_type, :system
+
+set :keep_releases, 3
+
+#before 'deploy', 'rvm1:install:rvm'  # install/update RVM
+
+namespace :deploy do
+
+  # desc 'Restart application'
+  # task :restart do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     # Your restart mechanism here, for example:
+  #     execute :touch, release_path.join('tmp/restart.txt')
+  #   end
+  # end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
+
