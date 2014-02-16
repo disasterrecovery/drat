2061c9acaa95ef57607decfd94d088c0cad351f7
Rylan Bowers <rylanb@gmail.com>
2014-02-15 17:13:08 -0700

    Updating Unicorn options.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -49,29 +49,17 @@ else
   before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
 
-    # Kill the old master as soon as our 1st worker is ready
-    # old_pid = "/srv/#{application}/shared/tmp/pids/unicorn.pid.oldbin"
-    # if File.exists?(old_pid) && server.pid != old_pid
-    #   begin
-    #     Process.kill("QUIT", File.read(old_pid).to_i)
-    #   rescue Errno::ENOENT, Errno::ESRCH
-    #     # someone else killed the old master already
-    #   end
-    # end
-
-    # Incremental kill-off
-    # old_pid = "#{server.config[:pid]}.oldbin"
-    # if old_pid != server.pid
-    #   begin
-    #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-    #     puts "Sending #{sig} signal to old unicorn master..."
-    #     Process.kill(sig, File.read(old_pid).to_i)
-    #   rescue Errno::ENOENT, Errno::ESRCH
-    #   end
-    # end
+    old_pid = "#{server.config[:pid]}.oldbin"
+    if File.exists?(old_pid) && server.pid != old_pid
+      begin
+        Process.kill("QUIT", File.read(old_pid).to_i)
+      rescue Errno::ENOENT, Errno::ESRCH
+        # someone else did our job for us
+      end
+    end
 
     # Throttle the master from forking too quickly (for incremental kill-off only)
-    #sleep 1
+    sleep 1
   end
 
   after_fork do |server, worker|
